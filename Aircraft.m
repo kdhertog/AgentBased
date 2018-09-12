186753425956e49ebf6db347a09f5df86edd8e33
Koen den Hertog <koendenhertog@icloud.com>
2018-09-12 12:16:15 +0200

    Added turn function

--- Aircraft.m
+++ Aircraft.m
@@ -4,7 +4,9 @@ classdef Aircraft
         position
         velocity
         vision
-        seperation 
+        seperation
+        turncount
+        heading
     end
     
     methods
@@ -18,15 +20,44 @@ classdef Aircraft
             %position_y] [position_x+200, position_y] [position_x-200, position_y-200]
             %[position_x, position_y-200] [position_x-200, position_y-200]]
             
-            angle = (2*pi).*rand;
-            obj.velocity = [cos(angle), sin(angle)];
+            obj.heading = (2*pi).*rand;
+            obj.velocity = [cos(obj.heading), sin(obj.heading)];
         
             obj.vision = 50;
             obj.seperation = 20;
             
         end
         
+        function obj = reactiveturn(obj, near_aircraft)
+            if (near_aircraft) && (obj.turncount <= 0)
+                
+                obj.heading = obj.heading + pi/6;
+                obj.velocity = [cos(obj.heading), sin(obj.heading)];
+                obj.turncount = 10;
+            
+            else %Either their are no aircraft nearby, or the aircraft turned recently
+                
+                obj.turncount = obj.turncount - 1;
+                
+            end
+            
+        end
         
+        function obj = proactiveturn(obj, near_aircraft_list)
+            
+            for i = 1:length(near_aircraft_list)
+                
+                near_aircraft_relative_postition = %relative position of the near aircraft (0 - 2pi)
+                %positionlist.append(near_aircraft_relative_postition)
+                    
+            end
+            
+            %For each 5 degrees of heading count the amount of aircraft and their distace
+            %Assign a heading score for each aircraft
+            %Change heading to heading with the best heading score
+          
+        end
+               
         function obj = update(obj)
            position_x = obj.position(1) + obj.velocity(1);
            position_y = obj.position(2) + obj.velocity(2);
