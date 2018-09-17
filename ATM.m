00d1bc995256ad8d47f00f7469998c2eb686da96
Koen den Hertog <koendenhertog@icloud.com>
2018-09-17 10:28:54 +0200

    Added lin 11

--- ATM.m
+++ ATM.m
@@ -7,7 +7,8 @@ for i=1:aircraft_count
 end
 
 manouvre=Manouvre(aircraft,[airspace_size, airspace_size]);
-f= figure;
+f= figure();
+%f= figure('visible','off'); %For an invisible figure
 airspace=Airspace(f,[airspace_size airspace_size],aircraft);
 manouvre.run(airspace);
 
