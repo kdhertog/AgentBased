252cd72679efd81e507b8cb39737140645c8023b
Koen den Hertog <koendenhertog@icloud.com>
2018-09-19 11:01:19 +0200

    Did some clean up of the code

--- Aircraft.m
+++ Aircraft.m
@@ -19,7 +19,7 @@ classdef Aircraft
             % other side of the border.
         
             
-            obj.heading = (2*pi).*rand; %Aircraft are spawned with a random heading
+            obj.heading = (2*pi)*rand; %Aircraft are spawned with a random heading
             obj.velocity = [cos(obj.heading), sin(obj.heading)]; % The velocity vector has an x & y component
         
             obj.vision = 50; % vision of the aircraft
@@ -31,65 +31,60 @@ classdef Aircraft
         function obj = reactiveturn(obj, near_aircraft)
             
             if near_aircraft == 1 
-                if obj.turncount <= 0
+                if obj.turncount <= 0 %Only turn if the required time between turns has passed
                     obj.heading = obj.heading+pi/6; % Change the heading by 30 degrees
                     obj.velocity = [cos(obj.heading), sin(obj.heading)]; % update velocity
                     obj.turncount = 3; % The aircraft is now not allowed to turn for 3 ticks
                 else
-                    obj.turncount = obj.turncount - 1;
+                    obj.turncount = obj.turncount - 1; %No turn, so time until next turn can go down
                 end
             else
-                obj.turncount = obj.turncount - 1;
+                obj.turncount = obj.turncount - 1; %No turn, so time until next turn can go down
             end
         end
         
         function obj = proactiveturn(obj, near_aircraft_list)
             near_aircraft_headings = []; %List of the relative position (angle) of the near aircraft
-            for i = 1:length(near_aircraft_list) %Near_aircraft_list compasses the id's of all near aircraft
+            for i = 1:length(near_aircraft_list) %Near_aircraft_list compasses all aircraft objects that are near
                 distance_aircraft=100000;
-                for j=1:length(obj.position)/2
+                
+                %The following section of code calculates the distance
+                %beween this aircraft and each near aircraft i. It does
+                %this by checking for each map what the distance is, and
+                %eventually choosing the shortest distance.
+                for j=1:length(obj.position)/2  %Check each map
                    distance_x_option = (near_aircraft_list(i).position(2*j-1)-obj.position(1));
                    distance_y_option = (near_aircraft_list(i).position(2*j)-obj.position(2));
                    distance_option=sqrt(distance_x_option^2+distance_y_option^2);
-                            
-                   if distance_option<distance_aircraft
+                   
+                   
+                   
+                   if distance_option<distance_aircraft %Check whether the current distance j is the smallest
                        distance_aircraft=distance_option;
                        distance_x = distance_x_option;
                        distance_y = distance_y_option;
-                   end
+                   end %The result will be the distance in x and y for the smallest total distance
                     
                 end
                  
                 near_aircraft_relative_position = atan2(distance_y,distance_x) + 2*pi*(distance_y<0); %For every near aircraft the relative position_angle gets calculated, and stored in a list
                 temp=[near_aircraft_headings,near_aircraft_relative_position];
-                near_aircraft_headings=temp; %In the end of the for loop, near_aircraft_headings compasses the relative positions off all near aircraft, tough they are not cooupled to a certain aircraft
+                near_aircraft_headings=temp; %In the end of the for loop, near_aircraft_headings compasses the relative positions off all near aircraft, tough they are not coupled to a certain aircraft
                      
             end
             
-            compass_divisions = 36;
+            compass_divisions = 36; %Amount of compass directions that will be checked for potential heading change
             divided_compass = 2*pi/compass_divisions;
             heading_score = [];
-            for i = 1:compass_divisions
+            for i = 1:compass_divisions %For each section of the compass, check the amount of aircraft in that direction, and store it in a list
                 temp1 = length(near_aircraft_headings(i*divided_compass-divided_compass <= near_aircraft_headings & near_aircraft_headings < i*divided_compass));
                 temp2 = [heading_score,temp1];
-                heading_score = temp2;
+                heading_score = temp2; %Heading score is a list of the amount of near aircraft for each section of the compass.
             end
-            
-%             [~, optimal_heading] = min(heading_score);
-%             obj.heading = deg2rad(optimal_heading * divided_compass);
-%             obj.velocity = [cos(obj.heading), sin(obj.heading)];
 
-            [~, worst_heading] = max(heading_score);
-            obj.heading = worst_heading * divided_compass+pi;
-            obj.velocity = [cos(obj.heading), sin(obj.heading)];
-            
-%             disp(obj.position)
-%             disp(heading_score)
-%             disp(optimal_heading)
-%             disp(" ")
-%             disp(" ")
-            
-            
+            [~, worst_heading] = max(heading_score); %Determine which section of the compass has the most aircraft
+            obj.heading = worst_heading * divided_compass+pi; %Change heading to the direction opposite of the worst one
+            obj.velocity = [cos(obj.heading), sin(obj.heading)]; %Update velocity
         end
                
         function obj = update(obj)
